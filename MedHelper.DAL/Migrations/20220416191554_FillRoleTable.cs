926afa80fa814c52950e629a3b35d86224ef440d
Pavlo Zabavskyi <71030079+pavlozab@users.noreply.github.com>
2022-04-26 20:03:49 +0300

    Revert "[UPD] delete MedHelperDB Context and generate new migrations"
    This reverts commit f06db18d5638a5f44872e1d89fa294626ec6f956.

---
+++ MedHelper.DAL/Migrations/20220416191554_FillRoleTable.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MedHelper.DAL.Migrations
+{
+    public partial class FillRoleTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleID", "UserRole" },
+                values: new object[] { 1, "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleID", "UserRole" },
+                values: new object[] { 2, "Doctor" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleID",
+                keyValue: 2);
+
+        }
+    }
+}
