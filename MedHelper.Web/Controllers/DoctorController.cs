3bb1193741b089f867abd96f0a34c8deb44548fd
rostyk-stakhiv <rostykstakhiv7@gmail.com>
2022-04-27 11:11:15 +0300

    Fixed Doctor Page

--- MedHelper.Web/Controllers/DoctorController.cs
+++ MedHelper.Web/Controllers/DoctorController.cs
@@ -1,21 +1,20 @@
-﻿using MedHelper.Web.Models;
+﻿using MedHelper.BLL.Interfaces;
+using MedHelper.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MedHelper.Web.Controllers
 {
     public class DoctorController : Controller
     {
+
+        private IDoctorService _doctorService;
+        public DoctorController(IDoctorService doctorService)
+        {
+            _doctorService = doctorService;
+        }
         public IActionResult Index()
         {
-            var doctor = new UserModel()
-            {
-                Id = 1,
-                FirstName = "Victor",
-                LastName = "Pro",
-                Email = "victor@gmail.com",
-                Patients = new List<PatientModel>() { new PatientModel() { Id=1, FirstName = "Ivan", LastName = "Petrov" } },
-                PatientsId = new List<int>() { 1 }
-            };
+            var doctor = _doctorService.GetById(2);
             ViewBag.Doctor = doctor;
             return View();
         }
