a4fea4fa97ab3c057f0fb21bbde1366253e2a3c7
Rostyk Stakhiv <71028410+rostyk-stakhiv@users.noreply.github.com>
2022-04-30 15:53:11 +0300

    Update MedHelperDBContext.cs

--- MedHelper.DAL/MedHelperDBContext.cs
+++ MedHelper.DAL/MedHelperDBContext.cs
@@ -23,8 +23,8 @@ namespace MedHelper.DAL
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder.UseNpgsql("Username=fwvunaemuwigvf;Password=900bf9214a0ce43234324145f6f98545a958f0864df0cf8b63b5b00f3d62cd8e;Host=ec2-52-30-67-143.eu-west-1.compute.amazonaws.com;Port=5432;Database=debigcnpahp883;SSL Mode=Require;Trust Server Certificate=true");
-            optionsBuilder.UseNpgsql("Username=postgres;Password=qwerty123;Host=localhost;Port=5432;Database=MedHelper;Trust Server Certificate=true");
+            optionsBuilder.UseNpgsql("Username=fwvunaemuwigvf;Password=900bf9214a0ce43234324145f6f98545a958f0864df0cf8b63b5b00f3d62cd8e;Host=ec2-52-30-67-143.eu-west-1.compute.amazonaws.com;Port=5432;Database=debigcnpahp883;SSL Mode=Require;Trust Server Certificate=true");
+            //optionsBuilder.UseNpgsql("Username=postgres;Password=qwerty123;Host=localhost;Port=5432;Database=MedHelper;Trust Server Certificate=true");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
